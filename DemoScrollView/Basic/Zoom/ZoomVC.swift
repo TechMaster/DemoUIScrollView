4082dc1b46a92e750e944ac5fe5cce659a6b55e8
Kien Nguyen Duc <duckien219@gmail.com>
2017-07-11 17:40:41 +0700

    #1 zoom image

--- DemoScrollView/Basic/Zoom/ZoomVC.swift
+++ DemoScrollView/Basic/Zoom/ZoomVC.swift
@@ -8,28 +8,30 @@
 
 import UIKit
 
-class ZoomVC: UIViewController {
+class ZoomVC: UIViewController,UIScrollViewDelegate {
+    
+    var photo = UIImageView()
 
+
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let imgView = UIImageView(image: UIImage(named: "miss2.jpg"))
+        imgView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        imgView.contentMode = .scaleAspectFit
+        scrollView.contentSize = CGSize(width: imgView.bounds.width, height: imgView.bounds.height)
+        photo = imgView
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 10
+        scrollView.delegate = self
+        self.scrollView.addSubview(imgView)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photo
     }
-    */
-
+    
+    
 }
