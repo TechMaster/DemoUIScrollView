93cacccf06ea217513afbedf161e43e3dea4d681
Cuong Trinh <cuong@techmaster.vn>
2017-07-12 10:58:08 +0700

    #1: Cuong toi uu code cho Kien

--- DemoScrollView/Basic/Zoom/ZoomVC.swift
+++ DemoScrollView/Basic/Zoom/ZoomVC.swift
@@ -10,22 +10,23 @@ import UIKit
 
 class ZoomVC: UIViewController,UIScrollViewDelegate {
     
-    var photo = UIImageView()
-
+    //var photo = UIImageView()
+    var photo: UIImageView!
 
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let imgView = UIImageView(image: UIImage(named: "miss2.jpg"))
-        imgView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-        imgView.contentMode = .scaleAspectFit
-        scrollView.contentSize = CGSize(width: imgView.bounds.width, height: imgView.bounds.height)
-        photo = imgView
+        //Cuong: chỉ cần nên dùng một biến trỏ tới UIImageView thôi
+        //let imgView = UIImageView(image: UIImage(named: "miss2.jpg"))
+        photo = UIImageView(image: UIImage(named: "miss2.jpg"))
+        photo.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        photo.contentMode = .scaleAspectFit
+        scrollView.contentSize = CGSize(width: photo.bounds.width, height: photo.bounds.height)
+        //photo = imgView
         scrollView.minimumZoomScale = 1
         scrollView.maximumZoomScale = 10
         scrollView.delegate = self
-        self.scrollView.addSubview(imgView)
+        self.scrollView.addSubview(photo)
         
     }
     
