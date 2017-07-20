51cef33cd4f1d8ee846d8a7d9c73c36752697423
Tung <tung@techmasters-iMac-2.local>
2017-07-20 14:43:32 +0700

    #4 NestedScrollView

--- DemoScrollView/Basic/NestedScrollView/Nested.swift
+++ DemoScrollView/Basic/NestedScrollView/Nested.swift
@@ -2,32 +2,37 @@
 //  Nested.swift
 //  DemoScrollView
 //
-//  Created by Tung on 7/12/17.
+//  Created by Tung on 7/20/17.
 //  Copyright © 2017 Techmaster. All rights reserved.
 //
 
 import UIKit
 
-class Nested: UIViewController, UIScrollViewDelegate {
-    
+class Nested: UIViewController,UIScrollViewDelegate {
+
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageController: UIPageControl!
+    
     var pageImages : [String] = []
     var photo : [UIImageView] = []
     var frontScrollViews : [UIScrollView] = []
-    
+
     var first = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pageImages = ["dog_1","dog_2","dog_3","dog_4","dog_5"]
+        pageImages = ["dog_1","dog_2","dog_3"]
         pageController.currentPage = 0
         pageController.numberOfPages = pageImages.count
-        
-        
+        view.bringSubview(toFront: pageController)
+        self.automaticallyAdjustsScrollViewInsets = false
+        self.scrollView.delegate = self
+
+        // Do any additional setup after loading the view.
     }
     override func viewDidLayoutSubviews() {
-        if !first {
+        if !first
+        {
             first = true
             let pagesScrollViewSize = scrollView.frame.size
             scrollView.contentSize = CGSize(width: pagesScrollViewSize.width * CGFloat(pageImages.count), height: 0)
@@ -37,6 +42,9 @@ class Nested: UIViewController, UIScrollViewDelegate {
                 imgView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
                 imgView.contentMode = .scaleAspectFit
                 
+                imgView.isUserInteractionEnabled = true
+                imgView.isMultipleTouchEnabled = true
+                
                 photo.append(imgView)
                 
                 let frontScrollView = UIScrollView(frame: CGRect(x: CGFloat(i) * scrollView.frame.size.width, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height))
@@ -44,6 +52,7 @@ class Nested: UIViewController, UIScrollViewDelegate {
                 frontScrollView.maximumZoomScale = 2
                 frontScrollView.delegate = self
                 frontScrollView.contentSize = imgView.bounds.size
+                
                 frontScrollView.addSubview(imgView)
                 frontScrollViews.append(frontScrollView)
                 self.scrollView.addSubview(frontScrollView)
@@ -51,15 +60,15 @@ class Nested: UIViewController, UIScrollViewDelegate {
         }
         
     }
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView?{
         return photo[pageController.currentPage]
-    }
-    @IBAction func pull(_ sender: Any) {
-        scrollView.contentOffset = CGPoint(x: CGFloat(pageController.currentPage) * scrollView.frame.size.width, y: 0)
     }
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
         pageController.currentPage = Int(pageIndex)
-        
+    }
+    @IBAction func onChange(_ sender: UIPageControl) {
+        scrollView.contentOffset = CGPoint(x: CGFloat(pageController.currentPage) * scrollView.frame.size.width, y: 0)
     }
 }
